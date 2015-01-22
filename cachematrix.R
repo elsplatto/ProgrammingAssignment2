125ef2f91d17d7936c41df53dce65520b46da173
Jason Taikato <jasontaikato@gmail.com>
2015-01-22 11:20:37 +1100

    More verbose commenting

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##=========makeCacheMatrix=================
+#The makeCacheMatrix function is a 'parent' function that contains a list of functions 
+#that do the grunt work 
+#It sets and gets the matrix in both it's original and inverted forms
+#It doesn't actually invert the matrix - the onus of that is handled in 
+#cacheSolve
+
+##=========cacheSolve=================
+#The cacheSolve function inverts the matrix
+#It also checks to see if the matrix has already been inverted or not
+#and returns the cached inverted matrix if it has already been computed
+#or inverts the matrix and returns it if it has not yet been inverted
+
+
+##Once the makeCacheMatrix has been assigned you can call it's child functions using the $ syntax
+
+##Eg. 
+#a<-makeCacheMatrix()
+#a$set(matrix(1:4,2,2))
+#a$get()
+#returns the following - matrix in it's original form
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+
+#to invert and set do the following
+#cacheSolve(a)
+#returning inverted matrix
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
 ## Write a short comment describing this function
 
 #makeCacheMatrix creates list of functions to
-# set  matrix
+# set matrix
 # get matrix
 # set inverted matrix
 # get inverted matrix
@@ -26,14 +58,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 # cacheSolve inverts the matrix (by calling the solve function)
-#in the first part of the function it checks to see if the matrix has already been inverted
+#in the first part of the function it checks to see if the inverted matrix has already been inverted
 #and returns it - if not it sets the inverted matrix
 
 cacheSolve <- function(x, ...) {
   
   m<-x$getmatrix()
   if(!is.null(m)){
-    message('getting cached data')
+    #getting cached matrix
     return(m)
   }
   matrix<-x$get()
